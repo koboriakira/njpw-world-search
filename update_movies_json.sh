c61c7542091c03bc3d8ce15aa8c11096ec51be76
Akira Kobori <private.beats@gmail.com>
2020-10-14 12:49:15 +0900

    shellを編集

--- update_movies_json.sh
+++ update_movies_json.sh
@@ -1,7 +1,10 @@
 cd ~/git/njpw-world-search && \
 python -m njpw_world_search.generate_json && \
-cp movies.json /Users/canary/git/njpw-world-search-front/src/json/movies.json && \
-cd ~/git/njpw-world-search-front && \
-git add src/json/movies.json && \
+git add movies.json && \
 git cm "Auto update movies.json" && \
 git push
+# cp movies.json /Users/canary/git/njpw-world-search-front/src/json/movies.json && \
+# cd ~/git/njpw-world-search-front && \
+# git add src/json/movies.json && \
+# git cm "Auto update movies.json" && \
+# git push
