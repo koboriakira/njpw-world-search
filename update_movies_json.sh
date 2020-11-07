67c246060526c697410ae02683171be833ce1baa
Akira Kobori <private.beats@gmail.com>
2020-11-08 01:00:38 +0900

    動画リストを更新

--- update_movies_json.sh
+++ update_movies_json.sh
@@ -4,8 +4,3 @@ python -m generate_json && \
 git add movies.json && \
 git cm "Auto update movies.json" && \
 git push
-# cp movies.json /Users/canary/git/njpw-world-search-front/src/json/movies.json && \
-# cd ~/git/njpw-world-search-front && \
-# git add src/json/movies.json && \
-# git cm "Auto update movies.json" && \
-# git push
