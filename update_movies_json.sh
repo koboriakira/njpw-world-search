1367fb995c9aef455d7af86e6fe39c9e2a23f1b0
Akira Kobori <private.beats@gmail.com>
2020-11-09 06:44:06 +0900

    シェルを編集

--- update_movies_json.sh
+++ update_movies_json.sh
@@ -1,6 +1,6 @@
-cd ~/git/njpw-world-search && \
-source .venv/bin/activate && \
-python -m generate_json && \
-git add movies.json && \
-git cm "Auto update movies.json" && \
+cd ~/git/njpw-world-search
+source .venv/bin/activate
+python -m generate_json
+git add json
+git cm "Auto update movies.json"
 git push
